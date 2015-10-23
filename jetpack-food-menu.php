ada1c781aee19fd163bc9e2a6085c8a0047accd1
rgvwebpro <contact@rgvwebpro.com>
2015-10-23 08:31:17 -0500

    Updated plugin information

--- jetpack-food-menu.php
+++ jetpack-food-menu.php
@@ -1,11 +1,24 @@
 <?php
 /*
 Plugin Name: RGVWEBPRO Food Menu
-Plugin URI: https://github.com/rgvwebpro/rgvwebpro-food-menu
+Plugin URI:  https://github.com/rgvwebpro/rgvwebpro-food-menu
 Description: Add food menus to your theme by using this plugin. Needs Jetpack to work.
-Author: RGVWEBPRO
-Version: 1.6
-Author URI: http://rgvwebpro.com/
+Version:     0.1-alpha
+Author:      RGVWEBPRO
+Author URI:  http://rgvwebpro.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: rgvwebpro_food_menu
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+RSS by RGVWEBPRO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with RSS by RGVWEBPRO. If not, see {License URI}.
 */
 
 //  Custom Post Type - Food Menu
